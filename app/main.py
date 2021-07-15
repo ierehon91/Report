aa387bb3a5eac5bc030254d94c3b32423414a9db
Andrey Plekhanov <ierehon91@gmail.com>
2021-07-15 14:32:37 +0300

    Небольшие исправления. Добавлены описания методов класса UpdateGibrit. Добавлено описание класса UpdateGibrit в документацию.

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ import app.update.kapella as update_kapella
 import app.update.pvd3 as update_pvd3
 import app.update.gibrit as update_gibrit
 
-day = 6
+day = 14
 month = 7
 year = 2021
 
@@ -15,19 +15,19 @@ gibrit_login = 'sml-admin'
 gibrit_password = '123456'
 gibrit_url = '10.10.251.2:9000'
 
-kapella = update_kapella.UpdateKapella(r'..\temp\kapella_data.csv', delimiter='$')
-kapella_data = kapella.get_data(year, month, day)
-
-pvd3 = update_pvd3.UpdatePvd3(pvd3_url)
-pvd3.authorization(pvd3_username, pvd3_password)
-pvd3.set_filial_number(pvd3_filial_number)
-pvd3_data = pvd3.get_pvd_data(year, month, day)
-
-# gibrit = update_gibrit.UpdateGibrit(gibrit_url)
-# gibrit.authorization_gibrit(gibrit_login, gibrit_password)
-# gibrit_data = gibrit.filial_gibrit_data(year, month, day, alias='Семилуки')
-# gibrit.close()
+# kapella = update_kapella.UpdateKapella(r'..\temp\kapella_data.csv', delimiter='$')
+# kapella_data = kapella.get_data(year, month, day)
 #
-print(kapella_data)
+# pvd3 = update_pvd3.UpdatePvd3(pvd3_url)
+# pvd3.authorization(pvd3_username, pvd3_password)
+# pvd3.set_filial_number(pvd3_filial_number)
+# pvd3_data = pvd3.get_pvd_data(year, month, day)
+
+gibrit = update_gibrit.UpdateGibrit(gibrit_url)
+gibrit.authorization(gibrit_login, gibrit_password)
+gibrit_data = gibrit.get_gibrit_data(year, month, day)
+gibrit.close()
+
+# print(kapella_data)
 # print(pvd3_data)
-# print(gibrit_data)
+print(type(gibrit_data))
--- app/update/gibrit.py
+++ app/update/gibrit.py
@@ -13,17 +13,21 @@ class UpdateGibrit():
         self.session = requests.Session()
 
     def _get_login_url(self) -> str:
+        """Получение ссылки для авторизации"""
         return f'http://{self.url}/login'
 
     def _login_data(self, login, password):
+        """Получение данных для авторизации"""
         self.login = login
         self.password = password
         return {'username': f'{self.login}', 'password': f'{self.password}'}
 
-    def authorization_gibrit(self, login, password):
+    def authorization(self, login, password):
+        """Метод авторизации на сервере системы Гибрит"""
         self.session.post(url=self._get_login_url(), data=self._login_data(login, password))
 
     def _get_report_url(self, year, month, day):
+        """Получение url для get запроса данных с сервера Гибрит"""
         report_url = f'http://{self.url}/hybrid/v2/check/analytics/all' \
                  '?sortField=paymentId' \
                  f'&fromPaymentDate={get_str_date_2(year, month, day)}' \
@@ -35,14 +39,17 @@ class UpdateGibrit():
                  'PAYMENT_ERROR_NOT_MONEY_BACK'
         return report_url
 
-    def _get_parse_data(self, year, month, day):
+    def _get_parse_data(self, year: int, month: int, day: int) -> object:
+        """Процесс парсинга данных из системы Гибрит"""
         return self.session.get(url=self._get_report_url(year, month, day))
 
-    def get_gibrit_data(self, year, month, day):
+    def get_gibrit_data(self, year: int, month: int, day: int) -> list:
+        """Получение данных из системы Гибрит для всех филиалов"""
         data = self._get_parse_data(year, month, day)
         return json.loads(data.text)
 
-    def filial_gibrit_data(self, year, month, day, alias=''):
+    def filial_gibrit_data(self, year: int, month: int, day: int, alias: str = '') -> list:
+        """Получение данных из системы Гибрит для конкретного филиала"""
         filial_data = []
         for row in self.get_gibrit_data(year, month, day):
             if alias in row['terminalName']:
@@ -50,4 +57,5 @@ class UpdateGibrit():
         return filial_data
 
     def close(self):
+        """Выход из системы Гибрит"""
         self.session.get(f'http://{self.url}/logout')
