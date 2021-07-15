5bc266833bbca70ebbe03e40bd92a88735e13521
Andrey Plekhanov <ierehon91@gmail.com>
2021-07-15 14:59:08 +0300

    Выделен отдельный файл config для хранения конфигаруционных данных

--- app/main.py
+++ app/main.py
@@ -1,33 +1,25 @@
 import app.update.kapella as update_kapella
 import app.update.pvd3 as update_pvd3
 import app.update.gibrit as update_gibrit
+import config
 
 day = 14
 month = 7
 year = 2021
 
-pvd3_url = '10.36.35.13'
-pvd3_username = 'i.merkulov',
-pvd3_password = 'zmr00A'
-pvd3_filial_number = 'MFC-000002595'
+kapella = update_kapella.UpdateKapella(r'..\temp\kapella_data.csv', delimiter='$')
+kapella_data = kapella.get_data(year, month, day)
 
-gibrit_login = 'sml-admin'
-gibrit_password = '123456'
-gibrit_url = '10.10.251.2:9000'
+pvd3 = update_pvd3.UpdatePvd3(config.pvd3_url)
+pvd3.authorization(config.pvd3_username, config.pvd3_password)
+pvd3.set_filial_number(config.pvd3_filial_number)
+pvd3_data = pvd3.get_pvd_data(year, month, day)
 
-# kapella = update_kapella.UpdateKapella(r'..\temp\kapella_data.csv', delimiter='$')
-# kapella_data = kapella.get_data(year, month, day)
-#
-# pvd3 = update_pvd3.UpdatePvd3(pvd3_url)
-# pvd3.authorization(pvd3_username, pvd3_password)
-# pvd3.set_filial_number(pvd3_filial_number)
-# pvd3_data = pvd3.get_pvd_data(year, month, day)
-
-gibrit = update_gibrit.UpdateGibrit(gibrit_url)
-gibrit.authorization(gibrit_login, gibrit_password)
+gibrit = update_gibrit.UpdateGibrit(config.gibrit_url)
+gibrit.authorization(config.gibrit_login, config.gibrit_password)
 gibrit_data = gibrit.get_gibrit_data(year, month, day)
 gibrit.close()
 
-# print(kapella_data)
-# print(pvd3_data)
-print(type(gibrit_data))
+print(kapella_data)
+print(pvd3_data)
+print(gibrit_data)
