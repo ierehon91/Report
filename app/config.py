9928db823c7029abd922872f10c2df95c4748fab
Andrey Plekhanov <ierehon91@gmail.com>
2021-07-15 15:24:42 +0300

    Настройка репозитория

---
+++ app/config.py
@@ -0,0 +1,8 @@
+pvd3_url = '10.36.35.13'
+pvd3_username = 'i.merkulov',
+pvd3_password = 'zmr00A'
+pvd3_filial_number = 'MFC-000002595'
+
+gibrit_login = 'sml-admin'
+gibrit_password = '123456'
+gibrit_url = '10.10.251.2:9000'
